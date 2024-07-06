ad132dedea61645f309a2b2a385bdd80c4fa34c0
SemKoksharov <kokshe196@proton.me>
2024-07-06 16:56:50 +0200

    Sat Jul  6 04:56:50 PM CEST 2024 changed folder structure

---
+++ Java/algorithms_on_java/src/BubbleSort.java
@@ -0,0 +1,36 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] array = {52, 67, 34, 2, 9, 12, 69, 20, 3, 8, 123, 98, 1, 0};
+
+        System.out.println("Original: ");
+        printArray(array);
+
+        boolean isSorted = false;
+
+        while (! isSorted){
+            isSorted = true;
+            for (int i = 1; i < array.length; i++) {
+                if (array[i]< array[i-1]){
+                    int temp = array[i];
+                    array[i] = array[i-1];
+                    array[i-1] = temp;
+                    isSorted = false;
+                }
+            }
+            printArray(array);
+        }
+        System.out.println("Final result");
+        printArray(array);
+    }
+
+    public static void printArray(int[] array) {
+        System.out.print("[");
+        for (int i = 0; i < array.length; i++) {
+            if (i > 0) {
+                System.out.print(", ");
+            }
+            System.out.print(array[i]);
+        }
+        System.out.println("]");
+    }
+}
--- Java/algorithms_on_java/src/FindMinElement.java
+++ Java/algorithms_on_java/src/FindMinElement.java
@@ -1,4 +1,4 @@
-public class Main {
+public class FindMinElement {
     public static void main(String[] args) {
         int[] array = {52, 67, 34, 2, 9, 12, 69, 20, 3, 8, 123, 98, 1, 0};
 
